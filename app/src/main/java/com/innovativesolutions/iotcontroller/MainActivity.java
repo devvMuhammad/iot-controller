d0dd182010071eea3010fbd702e7dae556696ded
Muhammad Umair Javed Khawaja <umairkh2185@gmail.com>
2024-04-17 19:26:42 +0500

    first change

--- app/src/main/java/com/innovativesolutions/iotcontroller/MainActivity.java
+++ app/src/main/java/com/innovativesolutions/iotcontroller/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+//1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
