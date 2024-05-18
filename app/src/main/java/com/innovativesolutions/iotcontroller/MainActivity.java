62ea046165898d8a21061257271e5cc4690096a6
muhammad <muhammadaljoufi@gmail.com>
2024-05-18 21:58:58 +0500

    Styled the main activity

--- app/src/main/java/com/innovativesolutions/iotcontroller/MainActivity.java
+++ app/src/main/java/com/innovativesolutions/iotcontroller/MainActivity.java
@@ -19,44 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TabLayout mainTabLayout = findViewById(R.id.mainTabLayout);
-        ViewPager2 mainViewPager = findViewById(R.id.mainViewerPage);
-        MyFragmentAdapter fragmentMainAdapter = new MyFragmentAdapter(this);
-        mainViewPager.setAdapter(fragmentMainAdapter);
-        mainTabLayout.addTab(mainTabLayout.newTab().setText("All Devices"));
-        mainTabLayout.addTab(mainTabLayout.newTab().setText("Active Devices"));
-        mainTabLayout.addTab(mainTabLayout.newTab().setText("Inactive Devices"));
-        new TabLayoutMediator(mainTabLayout, mainViewPager,
-                (tab, position) -> {
-                    switch (position) {
-                        case 0:
-                            tab.setText("All");
-                            tab.setIcon(R.drawable.ic_launcher_foreground);
-                            break;
-                        case 1:
-                            tab.setText("Active");
-                            tab.setIcon(R.drawable.ic_launcher_foreground);
 
-                            break;
-                        case 2:
-                            tab.setText("Inactive");
-                            tab.setIcon(R.drawable.ic_launcher_foreground);
-
-                            break;
-                    }
-                }
-        ).attach();
-        for(int i = 0;i<mainTabLayout.getTabCount();i++) {
-        TabLayout.Tab tab = mainTabLayout.getTabAt(i);
-            assert tab != null;
-            tab.setCustomView(R.layout.custom_tab_resource);
-            TextView tabTextView = Objects.requireNonNull(tab.getCustomView()).findViewById(R.id.customTabView);
-            tabTextView.setText(tab.getText());
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
