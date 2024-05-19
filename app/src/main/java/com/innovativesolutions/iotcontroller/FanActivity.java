6a1a8b45839c8e772286daee2739f942e97b7df6
muhammad <muhammadaljoufi@gmail.com>
2024-05-19 12:27:12 +0500

    enchanced custom slider and added logic for slider and toggle switch

--- app/src/main/java/com/innovativesolutions/iotcontroller/FanActivity.java
+++ app/src/main/java/com/innovativesolutions/iotcontroller/FanActivity.java
@@ -1,9 +1,13 @@
 package com.innovativesolutions.iotcontroller;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +24,33 @@ public class FanActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // referencing
+        AppCompatSeekBar slider = findViewById(R.id.speedSlider);
+        TextView currentSpeed = findViewById(R.id.currentSpeed);
+        SwitchCompat fanSwitch = findViewById(R.id.fanSwitch);
+
+        // when switch off, the slider is disabled and vice versa
+        fanSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            System.out.println("toggle click");
+            slider.setEnabled(isChecked);
+        });
+
+        // slider change listener
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                currentSpeed.setText(String.valueOf(progress));
+                // send the speed to the device
+                // sendSpeed(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
     }
 }
